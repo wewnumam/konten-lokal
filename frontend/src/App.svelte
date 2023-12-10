3ad1b522498de797c7cad2705df73e62255cc1e1
Ahmad Adillaumam <44931897+wewnumam@users.noreply.github.com>
2023-12-10 13:06:29 +0700

    Add masonry

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,8 +2,22 @@
   import "bootstrap/dist/css/bootstrap.min.css";
   import "bootstrap/dist/js/bootstrap.bundle.js";
   import { onMount } from "svelte";
+  import Masonry from "masonry-layout";
+
+  let masonry;
+
+  // Callback to initialize Masonry after images are loaded
+  function initMasonry(container) {
+    masonry = new Masonry(container, {
+      percentPosition: true,
+      itemSelector: '.col-item'
+      // Other Masonry options can be added here
+    });
+    masonry.layout();
+  }
 
   let items = [];
+  let row;
 
   async function fetchData() {
     try {
@@ -12,6 +26,7 @@
       );
       const data = await response.json();
       items = data.items;
+      initMasonry(row);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -42,75 +57,177 @@
 
   <div class="album py-5 bg-body-tertiary">
     <div class="container">
-      {#if items.length > 0}
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        {#each items as item (item.id)}
-        <div class="col">
-          <div class="card shadow-sm">
-            <img
-              class="bd-placeholder-img card-img-top img-fluid"
-              src={`http://127.0.0.1:8090/api/files/${item.collectionId}/${item.id}/${item.thumbnail}`}
-              alt={item.title}
-              />
+      <div class="row" bind:this={row} use:initMasonry>
+        <div class="col-sm-6 col-lg-4 mb-4 col-item">
+          <div class="card">
+            <svg
+              class="bd-placeholder-img card-img-top"
+              width="100%"
+              height="200"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: Image cap"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+              ><title>Placeholder</title><rect
+                width="100%"
+                height="100%"
+                fill="#868e96"
+              /><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text
+              ></svg
+            >
             <div class="card-body">
+              <h5 class="card-title">Card title that wraps to a new line</h5>
               <p class="card-text">
-                {item.caption}
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
               </p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary"
-                    >View</button
-                  >
-                  <button type="button" class="btn btn-sm btn-outline-secondary"
-                    >Edit</button
-                  >
-                </div>
-                <small class="text-body-secondary">9 mins</small>
-              </div>
             </div>
           </div>
         </div>
-        {/each}
-      </div>
-      {:else}
-        <p>Loading...</p>
-      {/if}
-    </div>
-  </div>
-
-  {#if items.length > 0}
-    <div class="row">
-      {#each items as item (item.id)}
-        <div class="card col-3">
-          <img
-            src={`http://127.0.0.1:8090/api/files/${item.collectionId}/${item.id}/${item.thumbnail}`}
-            class="card-img-top"
-            alt={item.title}
-          />
-          <div class="card-body">
-            <h5 class="card-title">{item.title}</h5>
-            <p class="card-text">{item.caption}</p>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">{item.release}</li>
-            <li class="list-group-item">
-              {#each item.expand.tags as tag (tag.id)}
-                {tag.name}{#if item.expand.tags.length > 0},
-                {/if}{/each}
-            </li>
-          </ul>
-          <div class="card-body">
-            <a href={item.expand.author.link} class="card-link"
-              >{item.expand.author.name}</a
-            >
-            <a href={item.link} class="card-link">Store</a>
+        <div class="col-sm-6 col-lg-4 mb-4 col-item">
+          <div class="card p-3">
+            <figure class="p-3 mb-0">
+              <blockquote class="blockquote">
+                <p>A well-known quote, contained in a blockquote element.</p>
+              </blockquote>
+              <figcaption class="blockquote-footer mb-0 text-body-secondary">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </figcaption>
+            </figure>
           </div>
         </div>
-      {/each}
+        <div class="col-sm-6 col-lg-4 mb-4 col-item">
+          <div class="card">
+            <svg
+              class="bd-placeholder-img card-img-top"
+              width="100%"
+              height="200"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: Image cap"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+              ><title>Placeholder</title><rect
+                width="100%"
+                height="100%"
+                fill="#868e96"
+              /><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text
+              ></svg
+            >
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This card has supporting text below as a natural lead-in to
+                additional content.
+              </p>
+              <p class="card-text">
+                <small class="text-body-secondary"
+                  >Last updated 3 mins ago</small
+                >
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-4 col-item">
+          <div class="card text-bg-primary text-center p-3">
+            <figure class="mb-0">
+              <blockquote class="blockquote">
+                <p>A well-known quote, contained in a blockquote element.</p>
+              </blockquote>
+              <figcaption class="blockquote-footer mb-0 text-white">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        {#if items.length > 0}
+          {#each items as item (item.id)}
+            <div class="col-sm-6 col-lg-4 mb-4 col-item">
+              <div class="card text-center">
+                <img
+                  class="bd-placeholder-img card-img-top img-fluid"
+                  src={`http://127.0.0.1:8090/api/files/${item.collectionId}/${item.id}/${item.thumbnail}`}
+                  alt={item.title}
+                />
+                <div class="card-body">
+                  <h5 class="card-title">{item.title}</h5>
+                  <p class="card-text">
+                    {item.caption}
+                  </p>
+                  <p class="card-text">
+                    <small class="text-body-secondary"
+                      >Last updated 3 mins ago</small
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          {/each}
+        {:else}
+        <div class="col-sm-6 col-lg-4 mb-4 col-item">
+          <div class="card text-center">
+            <div class="card-body">
+              <p class="card-text">
+                Loading...
+              </p>
+            </div>
+          </div>
+        </div>
+        {/if}
+        <div class="col-sm-6 col-lg-4 mb-4 col-item">
+          <div class="card">
+            <svg
+              class="bd-placeholder-img card-img"
+              width="100%"
+              height="260"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: Card image"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+              ><title>Placeholder</title><rect
+                width="100%"
+                height="100%"
+                fill="#868e96"
+              /><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Card image</text
+              ></svg
+            >
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-4 col-item">
+          <div class="card p-3 text-end">
+            <figure class="mb-0">
+              <blockquote class="blockquote">
+                <p>A well-known quote, contained in a blockquote element.</p>
+              </blockquote>
+              <figcaption class="blockquote-footer mb-0 text-body-secondary">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4 mb-4 col-item">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">
+                This is another card with title and supporting text below. This
+                card has some additional content to make it slightly taller
+                overall.
+              </p>
+              <p class="card-text">
+                <small class="text-body-secondary"
+                  >Last updated 3 mins ago</small
+                >
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  {:else}
-    <p>Loading...</p>
-  {/if}
+  </div>
 </main>
 
 <footer class="text-body-secondary py-5">
